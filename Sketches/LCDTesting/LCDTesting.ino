e9fa04b8568e636c3b559327cc734e198f27aae7
Steven <stevenolsen7@gmail.com>
2015-01-28 18:46:17 -0800

    LCD Library Setup Working
    Setup Works, writing characters does not.

--- Sketches/LCDTesting/LCDTesting.ino
+++ Sketches/LCDTesting/LCDTesting.ino
@@ -17,18 +17,18 @@
  DB4	11		8 } setup.
  */
 
-#define ZEROS 	B0000	// Empty port register for convenience
-#define RS		2		// Register Select, 1: Data Register, 0: Instruction Register
-#define ENABLE	3		// Enable signal for writing or reading data
-#define INIT	B0011	// First initialization string sent 3 times to LCD
-#define FOURBIT	B0010	// Interface set to 4 bit. High bits sent first, low bits second.
-#define LCD_ON	B1100	// LCD on, precede with ZEROS command for high bits
-#define LCD_OFF	B1011	// LCD off, precede with ZEROS
-#define CLEAR	B0001	// LCD clear, precede with ZEROS
-#define ENTRY	B0110	// Entry Mode, precede with ZEROS
-#define FONT	B1100	// Set number of lines and font
+#define ZEROS 			B0000	// Empty port register for convenience
+#define RS					2	// Register Select, 1: Data Register, 0: Instruction Register
+#define ENABLE				3	// Enable signal for writing or reading data
+#define INIT			B0011	// First initialization string sent 3 times to LCD
+#define FOURBIT			B0010	// Interface set to 4 bit. High bits sent first, low bits second.
+#define LCD_ON			B1111	// LCD on, precede with ZEROS command for high bits
+#define LCD_OFF			B1000	// LCD off, precede with ZEROS
+#define CLEAR			B0001	// LCD clear, precede with ZEROS
+#define ENTRY_MODE		B0110	// ENTRY_MODE Mode, precede with ZEROS
+#define FUNCTION_SET	B1000	// Set number of lines and FUNCTION_SET
 
-const byte SETUP[] = {FOURBIT, B0010, FONT, ZEROS, LCD_OFF, ZEROS, CLEAR, ZEROS, ENTRY};
+const byte SETUP[] = {FOURBIT, B0010, FUNCTION_SET, ZEROS, LCD_OFF, ZEROS, CLEAR, ZEROS, ENTRY_MODE};
 
 void setup()
 {
@@ -43,7 +43,8 @@ void setup()
 
 	// Initialize LCD
 	delay(50); // 20 ms before LCD init
-	
+	pulse();
+
 	digitalWrite(RS, LOW);
 	PORTB = INIT;
 	delayMicroseconds(4500);
@@ -58,41 +59,60 @@ void setup()
 	delayMicroseconds(100);
 
 	PORTB = B0010;
-	PORTB = FONT;
-	delayMicroseconds(100);
+	pulse();
+	PORTB = FUNCTION_SET;
+	pulse();
 	
 	PORTB = ZEROS;
+	pulse();
 	PORTB = LCD_OFF;
-	delayMicroseconds(100);
+	pulse();
 
 	PORTB = ZEROS;
+	pulse();
 	PORTB = CLEAR;
+	pulse();
 	delay(4);
 	
 	PORTB = ZEROS;
-	PORTB = ENTRY;
-	delayMicroseconds(100);
+	pulse();
+	PORTB = ENTRY_MODE;
+	pulse();
 
 	// LCD Done
-	// PORTB = ZEROS;
-	// delayMicroseconds(2000);
-	// PORTB = LCD_ON;
-	digitalWrite(ENABLE, HIGH);
-	delayMicroseconds(100);
-	digitalWrite(ENABLE, LOW);
+	PORTB = ZEROS;
+	pulse();
+	PORTB = LCD_ON;
+	pulse();
+
+	PORTB = 0100;
+	pulse();
+	PORTB = 1101;
+	pulse();
+	PORTB = 0100;
+	pulse();
+	PORTB = 1101;
+	pulse();
+	PORTB = 0100;
+	pulse();
+	PORTB = 1101;
+	pulse();
 }
 
 void loop()
 {
-	PORTB = 0010;
-	PORTB = 0001;
-	digitalWrite(ENABLE, HIGH);
-	delayMicroseconds(100);
-	digitalWrite(ENABLE, LOW);
-	delay(1000);
+	// PORTB = 0010;
+	// pulse();
+	// PORTB = 0001;
+	// pulse();
+	//delay(1000);
 }
 
-void clear()
+void pulse()
 {
-	
+	delayMicroseconds(1);
+	digitalWrite(ENABLE, HIGH);
+	delayMicroseconds(1);
+	digitalWrite(ENABLE, LOW);	
+	delayMicroseconds(100);
 }
