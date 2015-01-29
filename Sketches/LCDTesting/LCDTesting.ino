190d05be419f28c88b346aab3c5d9c606b40498c
Steven <stevenolsen7@gmail.com>
2015-01-28 16:36:56 -0800

    Updated LCD Library
    Still Doesn't Work.

--- Sketches/LCDTesting/LCDTesting.ino
+++ Sketches/LCDTesting/LCDTesting.ino
@@ -19,14 +19,14 @@
 
 #define ZEROS 	B0000	// Empty port register for convenience
 #define RS		2		// Register Select, 1: Data Register, 0: Instruction Register
-#define READY	3		// Enable signal for writing or reading data
+#define ENABLE	3		// Enable signal for writing or reading data
 #define INIT	B0011	// First initialization string sent 3 times to LCD
 #define FOURBIT	B0010	// Interface set to 4 bit. High bits sent first, low bits second.
 #define LCD_ON	B1100	// LCD on, precede with ZEROS command for high bits
-#define LCD_OFF	B1000	// LCD off, precede with ZEROS
+#define LCD_OFF	B1011	// LCD off, precede with ZEROS
 #define CLEAR	B0001	// LCD clear, precede with ZEROS
-#define ENTRY	B0101	// Entry Mode, precede with ZEROS
-#define FONT	B1000	// Set number of lines and font
+#define ENTRY	B0110	// Entry Mode, precede with ZEROS
+#define FONT	B1100	// Set number of lines and font
 
 const byte SETUP[] = {FOURBIT, B0010, FONT, ZEROS, LCD_OFF, ZEROS, CLEAR, ZEROS, ENTRY};
 
@@ -36,33 +36,63 @@ void setup()
 	Serial.begin(115200);
 
 	// Pin Mapping
-	DDRB = B111111; // Enable Port C as output
-	DDRD = DDRD | B11111100; // Enable Port D as output without touching pins 0, 1.
+	DDRB = B111111; // Enable Port B as output
+	pinMode(RS, OUTPUT);
+	pinMode(ENABLE, OUTPUT);
+	//DDRD = DDRD | B11111100; // Enable Port D as output without touching pins 0, 1.
 
 	// Initialize LCD
-	while (millis() <= 60); // ensure 15 ms uptime before LCD init
-	//Serial.println(millis());
+	delay(50); // 20 ms before LCD init
+	
+	digitalWrite(RS, LOW);
 	PORTB = INIT;
 	delayMicroseconds(4500);
+	
 	PORTB = INIT;
 	delayMicroseconds(150);
+	
 	PORTB = INIT;
+	delayMicroseconds(100);
 	
-	// Setup LCD
-	for (int i = 0; i <= 8; i++) {
-		PORTB = SETUP[i];
-	}
+	PORTB = FOURBIT;
+	delayMicroseconds(100);
+
+	PORTB = B0010;
+	PORTB = FONT;
+	delayMicroseconds(100);
 	
-	// LCD Done
 	PORTB = ZEROS;
-	delay(1);
-	PORTB = LCD_ON;
+	PORTB = LCD_OFF;
+	delayMicroseconds(100);
+
+	PORTB = ZEROS;
+	PORTB = CLEAR;
+	delay(4);
+	
+	PORTB = ZEROS;
+	PORTB = ENTRY;
+	delayMicroseconds(100);
+
+	// LCD Done
+	// PORTB = ZEROS;
+	// delayMicroseconds(2000);
+	// PORTB = LCD_ON;
+	digitalWrite(ENABLE, HIGH);
+	delayMicroseconds(100);
+	digitalWrite(ENABLE, LOW);
 }
 
 void loop()
 {
 	PORTB = 0010;
-	delay(1);
 	PORTB = 0001;
+	digitalWrite(ENABLE, HIGH);
+	delayMicroseconds(100);
+	digitalWrite(ENABLE, LOW);
 	delay(1000);
+}
+
+void clear()
+{
+	
 }
